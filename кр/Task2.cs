7495a74be3f2dea5a148e82c504955d54bb8ab85
Евгения <jenekrasilnikova@gmail.com>
2026-03-27 14:19:16 +0300

    Add files via upload

--- кр/Task2.cs
+++ кр/Task2.cs
@@ -10,7 +10,7 @@ namespace CW5.Variant00
     {
         public class Number : INumber
         {
-            private double _real;
+            protected double _real;
 
 
             public double Real => _real;
@@ -31,18 +31,22 @@ namespace CW5.Variant00
 
             public virtual void Sum(INumber other)
             {
+                if (other == null) return;
+
                 double Summ = Real + other.Real;
                 _real = Summ;
             }
 
             public virtual void Sub(INumber other)
             {
+                if (other == null) return;
                 double Subm = Real - other.Real;
                 _real = Subm;
             }
 
             public virtual void Mul(INumber other)
             {
+                if (other == null) return;
                 double Mulm = Real * other.Real;
                 _real = Mulm;
             }
@@ -63,6 +67,40 @@ namespace CW5.Variant00
             }
 
 
+        }
+
+        public class ComplexNumber : Number, IComplexNumber
+        {
+            private double _imag;
+            public double Imaginary => _imag;
+            public int ISign
+            {
+                get
+                {
+                    if (_imag < 0) return -1;
+                    if (_imag > 0) return 1;
+                    return 0; //переопределить это сразу override
+                }
+            }
+            public override double Abs => Math.Abs(_real* _real + _imag*_imag);
+
+            //ComplexNumber cnum1 = new ComplexNumber(1,2); 1+2i
+            //ComplexNumber cnum1 = new ComplexNumber(1); 1+ 0i;
+            public ComplexNumber(double real, double imag = 0): base(real)
+            {
+                _imag = imag;
+            }
+            public ComplexNumber(INumber other) : base(other.Real)
+            {
+                if (other is ComplexNumber cnum)
+                {
+                    _imag = cnum.Imaginary;
+                }
+                else
+                {
+                    _imag = 0;
+                }
+            }
         }
     }
 }
